01c12af61a80adcad0fbe694a96cf60fc1b8072a
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-03 15:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 function removeFromCart(item) {
   
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item)
+    if (cart[i].hasOwnProperty(item))
       cart.splice(i, 1)
     else if ()
       return "That item is not in your cart."
