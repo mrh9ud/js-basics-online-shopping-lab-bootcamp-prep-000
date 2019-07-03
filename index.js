0a070bea14eac94dfa43813c4fc40557a4bb0d40
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-03 13:34:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ function viewCart() {
     if (cart.length === 0) 
       return "Your shopping cart is empty."
     else {
-      
+      var cartMsg = "In your cart, you have"
+      for (var i = 0; i < cart.length; i++) {
+        
+      }
     }
     
 }
