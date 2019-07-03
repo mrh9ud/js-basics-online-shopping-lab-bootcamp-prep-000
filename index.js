2f8abc8867c53f25902e881936918fbe607bf64d
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-03 15:34:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,10 +48,11 @@ function total() {
 function removeFromCart(item) {
   
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(item))  
-      cart.splice([i], 1)
- }
+    if (cart[i].itemName === item) {
+      cart.splice([i], 0)
+    }
     return cart
+  }
 }
 
 function placeOrder(cardNumber) {
