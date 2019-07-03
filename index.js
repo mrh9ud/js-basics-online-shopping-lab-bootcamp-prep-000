cf78f841110e8a714d4af849f45e92f4f42c0044
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-03 15:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart[i].splice([i], 1)
-      return cart
+      
     }
     
   }
