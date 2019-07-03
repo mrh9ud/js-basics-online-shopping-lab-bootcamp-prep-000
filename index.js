5aed386956c245d2ba82420c9e5805529476b442
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-03 15:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function placeOrder(cardNumber) {
   if (cardNumber !== undefined) 
     for (var i = 0; i < cart.length; i++) {
       cart.splice(0, cart.length)
+      return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
     }
   
   
